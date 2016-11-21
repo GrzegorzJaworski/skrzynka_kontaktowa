df5a86171ba4319f8368548ec28747ccbc5826b2
Grzegorz Jaworski <grzegorz@student.edu>
2016-11-21 23:45:02 +0100

    Grzegorz Jaworski

---
+++ skrzynka_kontaktowa/src/ContactBoxBundle/Entity/Email.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace ContactBoxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Email
+ *
+ * @ORM\Table(name="email")
+ * @ORM\Entity(repositoryClass="ContactBoxBundle\Repository\EmailRepository")
+ */
+class Email
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Person", inversedBy="email")
+     *
+     */
+    private $person;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Email
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Email
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set person
+     *
+     * @param \ContactBoxBundle\Entity\Person $person
+     * @return Email
+     */
+    public function setPerson(\ContactBoxBundle\Entity\Person $person = null)
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    /**
+     * Get person
+     *
+     * @return \ContactBoxBundle\Entity\Person 
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+}
---
+++ skrzynka_kontaktowa/src/ContactBoxBundle/Entity/Phone.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace ContactBoxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Phone
+ *
+ * @ORM\Table(name="phone")
+ * @ORM\Entity(repositoryClass="ContactBoxBundle\Repository\PhoneRepository")
+ */
+class Phone
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="number", type="integer")
+     */
+    private $number;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Person", inversedBy="phone")
+     *
+     */
+    private $person;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set number
+     *
+     * @param integer $number
+     * @return Phone
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    /**
+     * Get number
+     *
+     * @return integer 
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Phone
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set person
+     *
+     * @param \ContactBoxBundle\Entity\Person $person
+     * @return Phone
+     */
+    public function setPerson(\ContactBoxBundle\Entity\Person $person = null)
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    /**
+     * Get person
+     *
+     * @return \ContactBoxBundle\Entity\Person 
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+}
